ad90ffa0ad160249d843250035321d85d82b6656
Soyagvs <agustinmartinezyt@gmail.com>
2024-07-05 01:23:00 -0300

    add querys md:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,9 +23,9 @@ export const Navbar = () => {
     }, []); // [] significa que solo se ejecuta una vez al montar el componente
 
     return (
-        <header className="flex justify-center items-center">
+        <header className="flex justify-center items-center ">
             <nav
-                className="flex flex-col justify-center items-center mt-12"
+                className="flex flex-col justify-center items-center mt-12 "
             >
                 <p className="font-bold text-4xl">ATETA</p>
                 <div className="md:hidden flex items-center">
@@ -47,8 +47,18 @@ export const Navbar = () => {
                 </div>
                 <div
                     id="menu"
-                    className="hidden"
+                    className="hidden md:flex md:justify-around md:items-center md:gap-20 lg:gap-40"
                 >
+                    <div className="hidden md:flex md:justify-center md:items-center">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="3em"
+                            height="3em"
+                            viewBox="0 0 24 24">
+                            <path d="M17 8C8 10 5.9 16.17 3.82 21.34l1.89.66l.95-2.3c.48.17.98.3 1.34.3C19 20 22 3 22 3c-1 2-8 2.25-13 3.25S2 11.5 2 13.5s1.75 3.75 1.75 3.75C7 8 17 8 17 8" />
+                        </svg>
+                        <p className="font-bold text-3xl">ATETA</p>
+                    </div>
                     <a
                         href="/"
                         className="block md:inline-block text-center md:text-left py-2 md:py-0"
